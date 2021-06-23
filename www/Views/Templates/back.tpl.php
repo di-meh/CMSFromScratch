5655d7184e2047b8d29fb90d1488cc22507d9729
Mides <m1d3s@protonmail.com>
2021-06-23 14:02:57 +0200

    Updated framework + implemented framework in CMS

--- www/Views/Templates/back.tpl.php
+++ www/Views/Templates/back.tpl.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="FR">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Template de BACK</title>
 	<meta name="description" content="ceci est la description de ma page">
+	<link rel="stylesheet" href="framework/dist/style.css">
+	<script src="framework/dist/main.js"></script>
 </head>
+
 <body>
 	<header>
 		<h1>Template du back</h1>
@@ -13,4 +17,5 @@
 	<!-- intégration de la vue -->
 
 </body>
+
 </html>
--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="FR">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Template de FRONT</title>
 	<meta name="description" content="ceci est la description de ma page">
+	<link rel="stylesheet" href="framework/dist/style.css">
+	<script src="framework/dist/main.js"></script>
 </head>
+
 <body>
 	<header>
 		<h1>Template du front</h1>
 	</header>
 
 	<!-- intégration de la vue -->
-	<?php include $this->view ;?>
+	<?php include $this->view; ?>
 
 </body>
+
 </html>
