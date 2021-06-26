1ba6a14666c56fac6669584600ee05b0485e2022
lnjim <locman@locman.fr>
2021-06-26 19:43:24 +0200

    regler conflt

--- www/Views/home.view.php
+++ www/Views/home.view.php
@@ -11,9 +11,7 @@
 		if(isset($_SESSION['id'])){
 
 			echo "<a id'' href='editprofil'>Edition du profil</a><br/>";
-			echo "<a id='' href='logout'>Déconnexion</a>";
-			echo "<a id '' href='editprofil'>Edition du profil</a><br/>";
-			echo "<a id='' href='logout'>Déconnexion</a> <br>";
+			echo "<a id='' href='logout'>Déconnexion</a><br>";
 			echo "<a id='' href='page/all'>Voir mes pages</a> <br>";
 			echo "<a id='' href='page/add'>Ajouter une page</a>";
 
