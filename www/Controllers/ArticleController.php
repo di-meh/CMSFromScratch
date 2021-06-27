563c7024913b6a9fb1937f4e275ecf44cf4b9949
Alexmdz77 <alext77165@gmail.com>
2021-06-27 11:51:50 +0200

    add slug

--- www/Controllers/ArticleController.php
+++ www/Controllers/ArticleController.php
@@ -28,23 +28,57 @@ class ArticleController{
 
 		session_start();
 		if(!isset($_SESSION['id'])) header("Location:/login"); # si user non connecté => redirection
+		$user = $_SESSION['user'];
 
 		$view = new View("addArticle");
 		$article = new Article();
 
 		if (!empty($_POST)) {
-            $article->setTitle($_POST['title']);
-            $article->setSlug($_POST['slug']);
-            $article->setAuthor($_POST['author']);
-            $article->setBody($_POST['body']);
-            $article->save();
-			header("Location:/articles/add");
+			
+		    if (empty($errors)){
+				$article->setTitle($_POST['title']);
+				$article->setAuthor($user->getID());
+				$article->setContent($_POST['content']);
+				if (empty($_POST['content'])){
+					$view->assign("errors", ["Veuillez remplir tous les champs"]);
+				}else{
+					$article->setSlug('/' . $article->title2slug($_POST['title']));
+					if (empty($article->getAllBySlug($article->getSlug()))){
+						$article->save();
+						header("Location:/articles/add");
+					}else{
+						echo $article->getSlug();
+						$view->assign("errors", ["Veuillez changer le titre de votre article"]);
+					}
+				}
+            }else{
+                $view->assign("errors", $errors);
+            }
         }
 
 		$form = $article->formAddArticle();
 		$view->assign("form", $form);
 		
 	}
+
+	public function viewArticleAction(){
+        session_start();
+        if (!isset($_SESSION['id'])) header("Location:/login"); # si user non connecté => redirection
+
+        $user = $_SESSION['user'];
+
+        $article = new Article();
+
+        $view = new View("viewArticle");
+
+        $uriExploded = explode("?", $_SERVER["REQUEST_URI"]);
+
+        $uri = $uriExploded[0];
+
+        $articles = $article->getAllBySlug($uri);
+        $view->assign("article", $articles[0]);
+
+    }
 
 
 }
--- www/Core/Router.php
+++ www/Core/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Core;
 
+use App\Models\Article;
+
 class Router
 {
 	private $routes = [];
@@ -10,6 +12,9 @@ class Router
 	private $action;
 
 	public function __construct($uri){
+	    $article = new Article();
+
+
 		$this->setUri($uri);
 		if(file_exists($this->routesPath)){
 			//[/] => Array ( [controller] => Global [action] => default )
@@ -19,14 +24,26 @@ class Router
 
 				$this->setController($this->routes[$this->uri]["controller"]);
 				$this->setAction($this->routes[$this->uri]["action"]);
-			}else{
-				die("Chemin inexistant : 404");
-			}
+			}elseif (!empty($article->getAllBySlug($this->uri))){
+                $this->setController("ArticleController");
+                $this->setAction("viewArticle");
+
+            }else{
+				die("\nChemin inexistant : 404");
+            }
 
 		}else{
 			die("Le fichier routes.yml ne fonctionne pas !");
 		}
 	}
+
+    /**
+     * @return mixed
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
 
 	public function setUri($uri){
 		$this->uri = trim(mb_strtolower($uri));
