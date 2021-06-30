7ddbac61d8e47e3acc6546485ab4f1e486dce657
Mides <m1d3s@protonmail.com>
2021-06-30 14:30:32 +0200

    Added resetpwd in routes.yml + changed reset form

--- www/Views/resetpwd.view.php
+++ www/Views/resetpwd.view.php
@@ -1,8 +1,7 @@
 <section class="d-flex flex-direction-column flex-align-items-center flex-justify-content-center s-h-full">
 	<div class="card s-w-350">
-		<h6 class="card-title">Login</h6>
+		<h6 class="card-title">Réinitialisation du mot de passe</h6>
 		<div class="card-content"><?php App\Core\FormBuilder::render($form) ?>
-            <a href="/forgetpwd">Mot de passe oublié</a>      
         </div>
 	</div>
 </section>
