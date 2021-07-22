ee13983476a0e103780e5ee992f4ddfe51d950e1
MarinSH <marine.sml@hotmail.com>
2021-07-22 22:35:21 +0200

    again

--- www/Views/register.view.php
+++ www/Views/register.view.php
@@ -1,8 +1,9 @@
 <section class="d-flex flex-direction-column flex-align-items-center flex-justify-content-center s-w-full">
-	<div class="card-title">
+	<div class="card s-w-350 sp-my-3">
+		<div class="card-title">
                  <a href="../lbly-admin/adminview"><button class='btn btn-light'><span class='material-icons'>undo</span></button></a>
                  <h6>S'inscrire</h6>
-	</div>
+		</div>
 		<div class="card-content"><?php App\Core\FormBuilder::render($form) ?></div>
 	</div>
 	<!-- <section>
