f7b4ca234efd11ba45ffcaae5db86158aadd5d8f
MarinSH <marine.sml@hotmail.com>
2021-07-25 00:27:04 +0200

    Ihm article, page

--- www/Views/seePage.view.php
+++ www/Views/seePage.view.php
@@ -1,7 +1,14 @@
-<section>
-    <?php
-        echo $page["title"];
-        echo $page["content"];
-        ?>
-
+<section class="container-fluid">
+    <div class="row">
+        <div class="col-full">
+            <div class="card">
+                <div class="card-title">
+                    <h6><?php echo $article["title"]; ?></h6>
+                </div>
+                <div class="card-content">
+                    <?php echo $article["content"];?>
+                </div>
+             </div>
+        </div>
+    </div>
 </section>
