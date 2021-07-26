de5574e6f04949189d7ec7f251cad879463ec9fd
Alexmdz77 <alext77165@gmail.com>
2021-07-26 19:24:25 +0200

    fix orders page

--- www/Views/orders.view.php
+++ www/Views/orders.view.php
@@ -1,25 +1,12 @@
 <section class="container-fluid">
     <div class="row">
         <div class="col-full">
-            <?php if (isset($deletemodal)) :?>
-                <div class="card">
-                    <h6 class="card-title">Delete Modal</h6>
-                    <div class="card-content">
-                        <?php if (isset($formdelete)) : ?>
-                            <?php App\Core\FormBuilder::render($formdelete); ?>
-                        <?php endif; ?>
-                        <br/>
-                        <a id="" href="/lbly-admin/articles"><button class="btn btn-danger">Annuler</button></a>
-                    </div>
-                </div>
-                <br/>
-            <?php endif; ?>
             <div class="card">
                 <h6 class="card-title">Vos Commandes</h6>
                 <div class="card-content">
                     <?php if (isset($orders)) :?>
                         <?php if (empty($orders)) :?>
-                            <p>Vous n'avez pas créé d'article.</p>
+                            <p>il n'y a aucune commande.</p>
                         <?php else: ?>
                             <table class="table">
                             <thead>
